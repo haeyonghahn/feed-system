013e62b0922aab4d751bf392aa4674b2d6cb0c5b
haeyonghahn <yong80211@gmail.com>
2025-05-25 21:43:12 +0900

    feat: post 서비스 구현

---
+++ src/main/java/org/fastcampus/post/application/dto/CreateCommentRequestDto.java
@@ -0,0 +1,7 @@
+package org.fastcampus.post.application.dto;
+
+public record CreateCommentRequestDto(
+    Long postId,
+    Long authorId,
+    String content) {
+}
---
+++ src/main/java/org/fastcampus/post/application/dto/UpdateCommentRequestDto.java
@@ -0,0 +1,7 @@
+package org.fastcampus.post.application.dto;
+
+public record UpdateCommentRequestDto(
+    Long commentId,
+    Long authorId,
+    String content) {
+}
