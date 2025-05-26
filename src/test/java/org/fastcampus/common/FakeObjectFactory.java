931117a011a18703dd9829e6c7e1294deef840a6
haeyonghahn <yong80211@gmail.com>
2025-05-26 23:38:44 +0900

    feat: post 서비스 레이어 테스트 코드 작성

--- src/test/java/org/fastcampus/common/FakeObjectFactory.java
+++ src/test/java/org/fastcampus/common/FakeObjectFactory.java
@@ -1,5 +1,13 @@
 package org.fastcampus.common;
 
+import org.fastcampus.post.application.CommentService;
+import org.fastcampus.post.application.PostService;
+import org.fastcampus.post.application.interfaces.CommentRepository;
+import org.fastcampus.post.application.interfaces.LikeRepository;
+import org.fastcampus.post.application.interfaces.PostRepository;
+import org.fastcampus.post.repository.FakeCommentRepository;
+import org.fastcampus.post.repository.FakeLikeRepository;
+import org.fastcampus.post.repository.FakePostRepository;
 import org.fastcampus.user.application.UserRelationService;
 import org.fastcampus.user.application.UserService;
 import org.fastcampus.user.application.interfaces.UserRelationRepository;
@@ -11,9 +19,14 @@ public class FakeObjectFactory {
 
     private static final UserRepository fakeUserRepository = new FakeUserRepository();
     private static final UserRelationRepository fakeUserRelationRepository = new FakeUserRelationRepository();
+    private static final PostRepository fakePostRepository = new FakePostRepository();
+    private static final CommentRepository fakeCommentRepository = new FakeCommentRepository();
+    private static final LikeRepository fakeLikeRepository = new FakeLikeRepository();
 
     private static final UserService userService = new UserService(fakeUserRepository);
     private static final UserRelationService userRelationService = new UserRelationService(fakeUserRelationRepository, userService);
+    private static final PostService postService = new PostService(userService, fakePostRepository, fakeLikeRepository);
+    private static final CommentService commentService = new CommentService(userService, postService, fakeCommentRepository, fakeLikeRepository);
 
     public static UserService getUserService() {
         return userService;
@@ -21,5 +34,13 @@ public class FakeObjectFactory {
 
     public static UserRelationService getUserRelationService() {
         return userRelationService;
+    }
+
+    public static PostService getPostService() {
+        return postService;
+    }
+
+    public static CommentService getCommentService() {
+        return commentService;
     }
 }
---
+++ src/test/java/org/fastcampus/post/repository/FakePostRepository.java
@@ -0,0 +1,30 @@
+package org.fastcampus.post.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.fastcampus.post.application.interfaces.PostRepository;
+import org.fastcampus.post.domain.Post;
+
+public class FakePostRepository implements PostRepository {
+
+    private final Map<Long, Post> store = new HashMap<>();
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Post save(Post post) {
+        if (post.getId() != null) {
+            store.put(post.getId(), post);
+            return post;
+        }
+
+        long id = store.size() + 1;
+        Post newPost = new Post(id, post.getAuthor(), post.getContent());
+        store.put(id, newPost);
+        return newPost;
+    }
+}
