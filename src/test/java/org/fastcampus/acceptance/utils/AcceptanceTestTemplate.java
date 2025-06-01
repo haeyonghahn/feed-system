9c57cb2262e53ad1f00d88c781e3bbc999cb6b71
haeyonghahn <yong80211@gmail.com>
2025-06-02 00:18:51 +0900

    feat: 서비스 구현 완료

---
+++ src/test/java/org/fastcampus/acceptance/utils/AcceptanceTestTemplate.java
@@ -0,0 +1,22 @@
+package org.fastcampus.acceptance.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AcceptanceTestTemplate {
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+    @Autowired
+    private DataLoader dataLoader;
+
+    @BeforeEach
+    public void setUp() {
+        databaseCleanup.execute();
+        dataLoader.loadData();
+    }
+}
